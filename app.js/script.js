263c3132465c0d39436bb9ad82670269ca536e62
dee chao <chaoserady@gmail.com>
2023-10-30 09:57:03 -0500

    updated hero, nav, media queries

--- app.js/script.js
+++ app.js/script.js
@@ -7,7 +7,12 @@ const burgerBottom = document.querySelector(".burger_bottom");
 const nav = document.querySelector(".container_section_hero_nav");
 const navLinks = document.querySelectorAll(".container_section_hero_li");
 
+// Media Queries
+
+const desktopMedia = window.matchMedia("(min-width: 1280px");
+
 // Toggle Nav Menu with Burger
+
 burger.addEventListener("click", () => {
   nav.classList.toggle("active_nav");
   burgerTop.classList.toggle("burgerTopClose");
@@ -15,12 +20,31 @@ burger.addEventListener("click", () => {
   burgerBottom.classList.toggle("burgerBottomClose");
 });
 
-// Close Nav Menu after Links are clicked
+// Close Nav Menu after Links are clicked // Mobile
+
 navLinks.forEach((nl) => {
   nl.addEventListener("click", () => {
     nav.classList.remove("active_nav");
     burgerTop.classList.remove("burgerTopClose");
     burgerMiddle.classList.remove("burgerMiddleClose");
     burgerBottom.classList.remove("burgerBottomClose");
+  });
+});
+
+// Reset Links / Remove Active Class from links // Desktop
+
+const resetLinks = () => {
+  navLinks.forEach((nl) => {
+    nl.classList.remove("active_nav_links");
+  });
+};
+
+// Add Active Class to Clicked Links // Desktop
+
+navLinks.forEach((nl) => {
+  nl.addEventListener("click", (e) => {
+    resetLinks();
+    if (e.currentTarget && desktopMedia.matches === true)
+      nl.classList.add("active_nav_links");
   });
 });
